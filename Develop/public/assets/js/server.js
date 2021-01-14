9f3b092e8df162f76281ba83c936fdb9e5bf907b
Niki Catapano <nsc9605@gmail.com>
2021-01-14 14:28:09 -0500

    installed express and set up server.js to require data

---
+++ Develop/public/assets/js/server.js
@@ -0,0 +1,17 @@
+// Require express 
+const express = require('express');
+
+// Create server
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+// LISTENER -- Starts server
+app.listen(PORT, function() {
+    console.log("App is listening on PORT: " + PORT);
+});
