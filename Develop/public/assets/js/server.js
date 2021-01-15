7b1af57d233b46e0c144e20db5e8b3638f08aff4
Niki Catapano <nsc9605@gmail.com>
2021-01-15 15:53:06 -0500

    added routes for html

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,5 +1,7 @@
-// Require express 
+// Dependencies
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 // Create server
 const app = express();
@@ -8,7 +10,19 @@ const PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
+
+// HTML routes
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+// API routes
 
 
 // LISTENER -- Starts server
