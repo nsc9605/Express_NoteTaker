25bb014514dd4a4808d7dd5813a3b02e77300b69
Niki Catapano <nsc9605@gmail.com>
2021-01-15 22:49:42 -0500

    added variable to apiRoute.js and folder tree fix

--- Develop/public/routes/apiRoute.js
+++ Develop/public/routes/apiRoute.js
@@ -1,19 +1,21 @@
 // Require dependencies
-const fs = require('fs');
-const db = require('../db/db.json');
+const fs = require("fs");
+const db = require("../db/db.json");
 
 // get API routes
-module.exports = function(app) {
-    
-app.get("/api/notes", function(req, res) {
-    res.json();
-});
+module.exports = function (app) {
+  app.get("/api/notes", function (req, res) {
+    res.send(db);
+  });
 
-// post api routes
-app.post("/api/notes", function(req, res) {
+  // post api routes
+  app.post("/api/notes", function (req, res) {
     console.log(req.body);
     var newNote = {
-
-    }
-})
-}
+        
+    };
+    db.push(newNote);
+    res.send(newNote);
+  });
+  
+};
--- Develop/public/routes/htmlRoute.js
+++ Develop/public/routes/htmlRoute.js
@@ -1,12 +1,14 @@
-const fs = require('fs');
-const path = require('path');
+// Require dependencies
+const path = require("path");
 
 // Export modules
-module.exports = function(app) {
+module.exports = function (app) {
+  // GET html route
+  app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+  });
 
-    // GET html route
-    app.get("/notes", function(req, res) {
-        res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
-
-}
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
+};
